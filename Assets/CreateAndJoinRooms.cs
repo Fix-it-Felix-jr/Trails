7d0c7cea0a600d99a75d2f63412bb08e5990a5ca
Fix-it-Felix-jr <72710917+Fix-it-Felix-jr@users.noreply.github.com>
2022-01-23 20:29:27 +0100

    Multiplayer

--- Assets/CreateAndJoinRooms.cs
+++ Assets/CreateAndJoinRooms.cs
@@ -4,45 +4,53 @@ using UnityEngine;
 using Photon.Pun;
 using System;
 using System.Threading.Tasks;
+using UnityEngine.UI;
 
 public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
 {
+    public GameObject button;
+
+    public InputField createInput;
+    //string createInput="Default";
+    //createInput.text="Default";
+    public InputField joinInput;
+    //string joinInput="Default";
+    //joinInput.text="Default";
+
+    bool joined=false;
+
     // Start is called before the first frame update
-    async void Start()
+    void Start()
     {
-        await Task.Delay(1000);
         CreateRoom();
-        
-        while(true){
-            JoinRoom();
-            await Task.Delay(3000);
-        }
-
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         
     }
 
     public void CreateRoom(){
-        //PhotonNetwork.CreateRoom(createImput.text);
         PhotonNetwork.CreateRoom("Default");
+        //PhotonNetwork.CreateRoom(createInput);
     }
 
     public void JoinRoom(){
-        //PhotonNetwork.JoinRoom(joinInput.text);
         PhotonNetwork.JoinRoom("Default");
+        //PhotonNetwork.JoinRoom(joinInput);
     }
     public override void OnJoinedRoom(){
-        while(true){
-            if (PhotonNetwork.PlayerList.Length > 1){
-                PhotonNetwork.LoadLevel("Tunnel");
-            }
-        }
+
+        joined=true;
         
 
+    }
+    void Update(){
+        foreach (var playersName in PhotonNetwork.PlayerList)
+            {
+            //tell use each player who is in the room
+            Debug.Log(playersName + " is in the room");
+            button.gameObject.SetActive(false);
+        }
+        if (joined && PhotonNetwork.PlayerList.Length > 1){
+            PhotonNetwork.LoadLevel("Tunnel");
+            joined=false;
+        }
     }
 }
